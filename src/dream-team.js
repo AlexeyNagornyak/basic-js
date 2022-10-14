2ab1b87467141adfd259ef29c8997fa0de2b009f
AlexeyNagornyak <lksjnagornak@gmail.com>
2022-10-14 13:32:17 +0300

    feat: add comlete task Dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if(Array.isArray(members)) {
+    let team = members.map(el => {
+      if(typeof el == 'string' && el != ''){
+        return el.trim()[0].toUpperCase()
+      }
+    })
+    return team.sort().join('')
+  }else{
+    return false
+  }
 }
 
 module.exports = {
